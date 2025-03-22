525d6e6ac526cc537dee81053924007adca58a0f
Shashank R <rshashank.252001@gmail.com>
2025-03-22 16:44:30 +0530

    Feat: Restructing the files

--- main/http_server.c
+++ main/http_server.c
@@ -14,6 +14,7 @@
 #include "http_server.h"
 #include "tasks_common.h"
 #include "wifi_app.h"
+#include <cJSON.h>
 
 // Tag used for ESP serial console message
 static const char TAG[] = "http_server";
@@ -41,17 +42,6 @@ const esp_timer_create_args_t fw_update_reset_args = {
 };
 esp_timer_handle_t fw_update_reset;
 
-// Embedded files: JQuery, index.html, app.css and app.js and favicon.ico files
-// extern const uint8_t jquery_3_3_1_min_js_start[]    asm("_binary_jquery_3_3_1_min_js_start");
-// extern const uint8_t jquery_3_3_1_min_js_end[]      asm("_binary_jquery_3_3_1_min_js_end");
-// extern const uint8_t index_html_start[]             asm("_binary_index_html_start");
-// extern const uint8_t index_html_end[]               asm("_binary_index_html_end");
-// extern const uint8_t app_css_start[]                asm("_binary_app_css_start");
-// extern const uint8_t app_css_end[]                  asm("_binary_app_css_end");
-// extern const uint8_t app_js_start[]                 asm("_binary_app_js_start");
-// extern const uint8_t app_js_end[]                   asm("_binary_app_js_end");
-// extern const uint8_t favicon_ico_start[]            asm("_binary_favicon_ico_start");
-// extern const uint8_t favicon_ico_end[]              asm("_binary_favicon_ico_end");
 
 /**
  * Checks the g_fw_update_status and creates the fw_update_reset timer if g_fw_update_status is true.
@@ -119,80 +109,6 @@ static void http_server_monitor(void *parameter)
 	}
 }
 
-/**
- * JQuery get handler requested when accessing the web page
- * @param req http request for which the uri needs to be handled
- * @return ESP_OK
- */
-// static esp_err_t http_server_jquery_handler(httpd_req_t *req)
-// {
-// 	ESP_LOGI(TAG, "Jquery requested");
-
-// 	httpd_resp_set_type(req, "application/javascript");
-// 	httpd_resp_send(req, (const char *)jquery_3_3_1_min_js_start, jquery_3_3_1_min_js_end - jquery_3_3_1_min_js_start);
-
-// 	return ESP_OK;
-// }
-
-/**
- * Sends the index.html page.
- * @param req HTTP request for which the uri needs to be handled.
- * @return ESP_OK
- */
-// static esp_err_t http_server_index_html_handler(httpd_req_t *req)
-// {
-// 	ESP_LOGI(TAG, "index.html requested");
-
-// 	httpd_resp_set_type(req, "text/html");
-// 	httpd_resp_send(req, (const char *)index_html_start, index_html_end - index_html_start);
-
-// 	return ESP_OK;
-// }
-
-/**
- * app.css get handler is requested when accessing the web page.
- * @param req HTTP request for which the uri needs to be handled.
- * @return ESP_OK
- */
-// static esp_err_t http_server_app_css_handler(httpd_req_t *req)
-// {
-// 	ESP_LOGI(TAG, "app.css requested");
-
-// 	httpd_resp_set_type(req, "text/css");
-// 	httpd_resp_send(req, (const char *)app_css_start, app_css_end - app_css_start);
-
-// 	return ESP_OK;
-// }
-
-/**
- * app.js get handler is requested when accessing the web page.
- * @param req HTTP request for which the uri needs to be handled.
- * @return ESP_OK
- */
-// static esp_err_t http_server_app_js_handler(httpd_req_t *req)
-// {
-// 	ESP_LOGI(TAG, "app.js requested");
-
-// 	httpd_resp_set_type(req, "application/javascript");
-// 	httpd_resp_send(req, (const char *)app_js_start, app_js_end - app_js_start);
-
-// 	return ESP_OK;
-// }
-
-/**
- * Sends the .ico (icon) file when accessing the web page.
- * @param req HTTP request for which the uri needs to be handled.
- * @return ESP_OK
- */
-// static esp_err_t http_server_favicon_ico_handler(httpd_req_t *req)
-// {
-// 	ESP_LOGI(TAG, "favicon.ico requested");
-
-// 	httpd_resp_set_type(req, "image/x-icon");
-// 	httpd_resp_send(req, (const char *)favicon_ico_start, favicon_ico_end - favicon_ico_start);
-
-// 	return ESP_OK;
-// }
 
 /**
  * Receives the .bin file fia the web page and handles the firmware update
@@ -209,90 +125,114 @@ esp_err_t http_server_OTA_update_handler(httpd_req_t *req)
 	int recv_len;
 	bool is_req_body_started = false;
 	bool flash_successful = false;
+	int barWidth = 50;
+
+	// Enable CORS by adding headers
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Methods", "POST, OPTIONS");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Headers", "Content-Type");
+
+    // Handle CORS preflight request
+    if (req->method == HTTP_OPTIONS)
+    {
+        httpd_resp_send(req, NULL, 0);
+        return ESP_OK;
+    }
 
 	const esp_partition_t *update_partition = esp_ota_get_next_update_partition(NULL);
 
 	if (!update_partition)
     {
-        ESP_LOGI(TAG, "No OTA partition found");
+        ESP_LOGE(TAG, "No OTA partition found");
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "OTA Partition Not Found");
         return ESP_FAIL;
     }
 
-	do
-	{
-		// Read the data for the request
-		if ((recv_len = httpd_req_recv(req, ota_buff, MIN(content_length, sizeof(ota_buff)))) < 0)
-		{
-			// Check if timeout occurred
-			if (recv_len == HTTPD_SOCK_ERR_TIMEOUT)
-			{
-				ESP_LOGI(TAG, "http_server_OTA_update_handler: Socket Timeout");
-				continue; ///> Retry receiving if timeout occurred
-			}
-			ESP_LOGI(TAG, "http_server_OTA_update_handler: OTA other Error %d", recv_len);
-			return ESP_FAIL;
-		}
-		printf("http_server_OTA_update_handler: OTA RX: %d of %d\r", content_received, content_length);
+	ESP_LOGI(TAG, "Starting OTA update...\nOTA Update Start: Total Size = %d bytes", content_length);
 
-		// Is this the first data we are receiving
-		// If so, it will have the information in the header that we need.
-		if (!is_req_body_started)
-		{
-			is_req_body_started = true;
+	esp_err_t err = esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &ota_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Error starting OTA update: %s", esp_err_to_name(err));
+        return ESP_FAIL;
+    }
 
-			// Get the location of the .bin file content (remove the web form data)
-			char *body_start_p = strstr(ota_buff, "\r\n\r\n") + 4;
-			int body_part_len = recv_len - (body_start_p - ota_buff);
+    do {
+        // Read the data for the request
+        recv_len = httpd_req_recv(req, ota_buff, MIN(content_length - content_received, sizeof(ota_buff)));
 
-			printf("http_server_OTA_update_handler: OTA file size: %d\r\n", content_length);
+        if (recv_len < 0) {
+            if (recv_len == HTTPD_SOCK_ERR_TIMEOUT) {
+                ESP_LOGW(TAG, "Socket Timeout, retrying...");
+                continue; // Retry receiving if timeout occurred
+            }
+            ESP_LOGE(TAG, "OTA Receive Error: %d", recv_len);
+            return ESP_FAIL;
+        }
 
-			esp_err_t err = esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &ota_handle);
-			if (err != ESP_OK)
-			{
-				printf("http_server_OTA_update_handler: Error with OTA begin, cancelling OTA\r\n");
-				return ESP_FAIL;
-			}
-			else
-			{
-				printf("http_server_OTA_update_handler: Writing to partition subtype %d at offset 0x%lx\r\n", update_partition->subtype, update_partition->address);
-			}
+		// Calculate the progress as a fraction
+        float progress = (float)content_received / content_length;
+        int pos = barWidth * progress;
+        
+        // Print the progress bar
+        printf("[");
+        for (int i = 0; i < barWidth; i++) {
+            if (i < pos)
+                printf("#");
+            else
+                printf(" ");
+        }
+        printf("] %3d%%\r", (int)(progress * 100));
+        fflush(stdout);  // Ensure the progress is printed immediately
 
-			// Write this first part of the data
-			esp_ota_write(ota_handle, body_start_p, body_part_len);
-			content_received += body_part_len;
-		}
-		else
-		{
-			// Write OTA data
-			esp_ota_write(ota_handle, ota_buff, recv_len);
-			content_received += recv_len;
-		}
+        // Process header only for first received chunk
+        if (!is_req_body_started) {
+            is_req_body_started = true;
 
-	} while (recv_len > 0 && content_received < content_length);
+            // Find start of binary data in the request
+            char *body_start_p = strstr(ota_buff, "\r\n\r\n");
+            if (!body_start_p) {
+                ESP_LOGE(TAG, "Invalid request format: No header-body separator found!");
+                return ESP_FAIL;
+            }
+            body_start_p += 4; // Move past the separator
 
-	if (esp_ota_end(ota_handle) == ESP_OK)
-	{
-		// Lets update the partition
-		if (esp_ota_set_boot_partition(update_partition) == ESP_OK)
-		{
-			const esp_partition_t *boot_partition = esp_ota_get_boot_partition();
-			ESP_LOGI(TAG, "http_server_OTA_update_handler: Next boot partition subtype %d at offset 0x%lx", boot_partition->subtype, boot_partition->address);
-			flash_successful = true;
-		}
-		else
-		{
-			ESP_LOGI(TAG, "http_server_OTA_update_handler: FLASHED ERROR!!!");
-		}
-	}
-	else
-	{
-		ESP_LOGI(TAG, "http_server_OTA_update_handler: esp_ota_end ERROR!!!");
-	}
+            int body_part_len = recv_len - (body_start_p - ota_buff);
 
-	// We won't update the global variables throughout the file, so send the message about the status
-	if (flash_successful) { http_server_monitor_send_message(HTTP_MSG_OTA_UPDATE_SUCCESSFUL); } else { http_server_monitor_send_message(HTTP_MSG_OTA_UPDATE_FAILED); }
+            ESP_LOGI(TAG, "Writing to OTA partition at offset 0x%lx", update_partition->address);
 
-	return ESP_OK;
+            if (esp_ota_write(ota_handle, body_start_p, body_part_len) != ESP_OK) {
+                ESP_LOGE(TAG, "Error writing OTA data!");
+                return ESP_FAIL;
+            }
+            content_received += body_part_len;
+        } else {
+            // Write subsequent OTA data
+            if (esp_ota_write(ota_handle, ota_buff, recv_len) != ESP_OK) {
+                ESP_LOGE(TAG, "Error writing OTA data!");
+                return ESP_FAIL;
+            }
+            content_received += recv_len;
+        }
+
+    } while (recv_len > 0 && content_received < content_length);
+
+    if (esp_ota_end(ota_handle) == ESP_OK) {
+        if (esp_ota_set_boot_partition(update_partition) == ESP_OK) {
+            const esp_partition_t *boot_partition = esp_ota_get_boot_partition();
+            ESP_LOGI(TAG, "OTA Update Successful! Next boot partition at offset 0x%lx", boot_partition->address);
+            flash_successful = true;
+        } else {
+            ESP_LOGE(TAG, "Failed to set boot partition!");
+        }
+    } else {
+        ESP_LOGE(TAG, "OTA update failed!");
+    }
+
+    // Send OTA update status
+    http_server_monitor_send_message(flash_successful ? HTTP_MSG_OTA_UPDATE_SUCCESSFUL : HTTP_MSG_OTA_UPDATE_FAILED);
+
+    return flash_successful ? ESP_OK : ESP_FAIL;
+
 }
 
 /**
@@ -303,16 +243,19 @@ esp_err_t http_server_OTA_update_handler(httpd_req_t *req)
  */
 esp_err_t http_server_OTA_status_handler(httpd_req_t *req)
 {
-	char otaJSON[100];
+	cJSON *response = cJSON_CreateObject();
+    cJSON_AddStringToObject(response, "compile_date", __DATE__);
+    cJSON_AddStringToObject(response, "compile_time", __TIME__);
+    cJSON_AddNumberToObject(response, "ota_update_status", g_fw_update_status);
 
-	ESP_LOGI(TAG, "OTAstatus requested");
+    char *response_str = cJSON_Print(response);
+    httpd_resp_set_type(req, "application/json");
+    httpd_resp_send(req, response_str, strlen(response_str));
 
-	sprintf(otaJSON, "{\"ota_update_status\":%d,\"compile_time\":\"%s\",\"compile_date\":\"%s\"}", g_fw_update_status, __TIME__, __DATE__);
-
-	httpd_resp_set_type(req, "application/json");
-	httpd_resp_send(req, otaJSON, strlen(otaJSON));
-
-	return ESP_OK;
+    cJSON_Delete(response);
+    free(response_str);
+    
+    return ESP_OK;
 }
 
 /**
@@ -324,11 +267,12 @@ static httpd_handle_t http_server_configure(void)
 	// Generate the default configuration
 	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
+	// Create the message queue
+	http_server_monitor_queue_handle = xQueueCreate(3, sizeof(http_server_queue_message_t));
+	
 	// Create HTTP server monitor task
 	xTaskCreatePinnedToCore(&http_server_monitor, "http_server_monitor", HTTP_SERVER_MONITOR_STACK_SIZE, NULL, HTTP_SERVER_MONITOR_PRIORITY, &task_http_server_monitor, HTTP_SERVER_MONITOR_CORE_ID);
 
-	// Create the message queue
-	http_server_monitor_queue_handle = xQueueCreate(3, sizeof(http_server_queue_message_t));
 
 	// The core that the HTTP server will run on
 	config.core_id = HTTP_SERVER_TASK_CORE_ID;
@@ -356,50 +300,6 @@ static httpd_handle_t http_server_configure(void)
 	{
 		ESP_LOGI(TAG, "http_server_configure: Registering URI handlers");
 
-		// register query handler
-		// httpd_uri_t jquery_js = {
-		// 		.uri = "/jquery-3.3.1.min.js",
-		// 		.method = HTTP_GET,
-		// 		.handler = http_server_jquery_handler,
-		// 		.user_ctx = NULL
-		// };
-		// httpd_register_uri_handler(http_server_handle, &jquery_js);
-
-		// // register index.html handler
-		// httpd_uri_t index_html = {
-		// 		.uri = "/",
-		// 		.method = HTTP_GET,
-		// 		.handler = http_server_index_html_handler,
-		// 		.user_ctx = NULL
-		// };
-		// httpd_register_uri_handler(http_server_handle, &index_html);
-
-		// // register app.css handler
-		// httpd_uri_t app_css = {
-		// 		.uri = "/app.css",
-		// 		.method = HTTP_GET,
-		// 		.handler = http_server_app_css_handler,
-		// 		.user_ctx = NULL
-		// };
-		// httpd_register_uri_handler(http_server_handle, &app_css);
-
-		// // register app.js handler
-		// httpd_uri_t app_js = {
-		// 		.uri = "/app.js",
-		// 		.method = HTTP_GET,
-		// 		.handler = http_server_app_js_handler,
-		// 		.user_ctx = NULL
-		// };
-		// httpd_register_uri_handler(http_server_handle, &app_js);
-
-		// // register favicon.ico handler
-		// httpd_uri_t favicon_ico = {
-		// 		.uri = "/favicon.ico",
-		// 		.method = HTTP_GET,
-		// 		.handler = http_server_favicon_ico_handler,
-		// 		.user_ctx = NULL
-		// };
-		// httpd_register_uri_handler(http_server_handle, &favicon_ico);
 
         // register OTAupdate handler
 		httpd_uri_t OTA_update = {
@@ -410,10 +310,19 @@ static httpd_handle_t http_server_configure(void)
 		};
 		httpd_register_uri_handler(http_server_handle, &OTA_update);
 
+		//  Add handler for OPTIONS preflight requests
+		httpd_uri_t ota_options_uri = {
+			.uri = "/OTAupdate",
+			.method = HTTP_OPTIONS,
+			.handler = http_server_OTA_update_handler,
+			.user_ctx = NULL
+		};
+		httpd_register_uri_handler(http_server_handle, &ota_options_uri);
+		
 		// register OTAstatus handler
 		httpd_uri_t OTA_status = {
 				.uri = "/OTAstatus",
-				.method = HTTP_POST,
+				.method = HTTP_GET,
 				.handler = http_server_OTA_status_handler,
 				.user_ctx = NULL
 		};
